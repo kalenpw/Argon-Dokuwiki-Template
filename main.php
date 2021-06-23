45ce618ab7a1c89f040b124ac6d49053cc8de609
kalenpw <kalenpw@kalenpw.com>
2021-06-23 11:14:29 -0600

    Formatting and template info

--- main.php
+++ main.php
@@ -12,7 +12,6 @@ if (!defined('DOKU_INC')) {
 }
 /* must be run from within DokuWiki */
 @require_once dirname(__FILE__) . '/tpl_functions.php'; /* include hook for template functions */
-header('X-UA-Compatible: IE=edge,chrome=1');
 
 $showTools = !tpl_getConf('hideTools') || (tpl_getConf('hideTools') && !empty($_SERVER['REMOTE_USER']));
 $showSidebar = page_findnearest($conf['sidebar']) && ($ACT == 'show');
@@ -20,234 +19,247 @@ $showSidebar = page_findnearest($conf['sidebar']) && ($ACT == 'show');
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-        <link rel="icon" type="image/png" href="assets/img/favicon.png">
-        <title>
-            <?php tpl_pagetitle()?> [
-            <?php echo strip_tags($conf['title']) ?>]
-        </title>
-        <?php tpl_metaheaders()?>
-<?php echo tpl_favicon(array(
-    'favicon',
-    'mobile',
-))
-?>
 
-        <?php tpl_includeFile('meta.html')?>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+    <title>
+        <?php tpl_pagetitle() ?> [
+        <?php echo strip_tags($conf['title']) ?>]
+    </title>
+    <?php tpl_metaheaders() ?>
+    <?php echo tpl_favicon(array(
+        'favicon',
+        'mobile',
+    ))
+    ?>
 
-        <!-- 
+    <?php tpl_includeFile('meta.html') ?>
+
+    <!-- 
         I know the CSS and JS imports can be done within the style.ini and script.js files,
         but I had some issues with styling (and import order) there, so I'm doing those imports here. 
         -->
-        <!--     Fonts and icons  -->
-        <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet"> -->
-        <link href="<?php echo tpl_basedir(); ?>assets/css/fonts.css" rel="stylesheet">
-        <!-- CSS Files -->
-        <link href="<?php echo tpl_basedir(); ?>assets/css/doku.css" rel="stylesheet" />
-        <!-- JS -->
-        <script src="<?php echo tpl_basedir(); ?>assets/js/core/bootstrap.min.js" type="text/javascript"></script>
-        <script src="<?php echo tpl_basedir(); ?>assets/js/argon-design-system.min.js" type="text/javascript"></script>
+    <!--     Fonts and icons  -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet"> -->
+    <link href="<?php echo tpl_basedir(); ?>assets/css/fonts.css" rel="stylesheet">
+    <!-- CSS Files -->
+    <link href="<?php echo tpl_basedir(); ?>assets/css/doku.css" rel="stylesheet" />
+    <!-- JS -->
+    <script src="<?php echo tpl_basedir(); ?>assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+    <script src="<?php echo tpl_basedir(); ?>assets/js/argon-design-system.min.js" type="text/javascript"></script>
 
-        <style>
-            .kpw-header {
-                background-color: #343a40 !important;
-            }
-            .kpw-header-logo {
-                font-size: 20px;
-                color: white !important;
-            }
-            .kpw-header-logo:hover {
-                text-decoration: none !important;
-                color: #eee !important;
-            }
-            .kpw-footer {
-                background-color: #343a40 !important;
-                border-radius: 0 !important;
-                flex-shrink: 0 !important;
-            }
-            .kpw-footer svg {
-                fill: white !important;
-            }
-            .kpw-footer svg:hover {
-                fill: #ccc !important;
-            }
-            .kpw-footer li:hover {
-                fill: #ccc !important;
-            }
-            .kpw-footer a:hover {
-                color: #ccc !important;
-                fill: #ccc !important;
-            }
-            .kpw-full-menu {
-                list-style-type: none;
-                display: inline !important;
-            }
-            .kpw-full-menu li{
-                display: inline !important;
-            }
-            .kpw-full-menu li a{
-                display: inline !important;
-            }
-            html, body {
-                height: 100% !important;
-            }
-            body {
-                display: flex !important;
-                flex-direction: column !important;
-            }
-            .kpw-content-wrapper {
-                flex: 1 0 auto !important;
-            }
-        </style>
-    
-    </head>
+    <style>
+        /* sticky footer */
+        html,
+        body {
+            height: 100% !important;
+        }
 
-    <body class="docs ">
-        <div class="kpw-content-wrapper" id="dokuwiki__site">
+        body {
+            display: flex !important;
+            flex-direction: column !important;
+        }
 
+        /* kalen overrides  */
+        .kpw-header {
+            background-color: #343a40 !important;
+        }
 
-            <header
-                class="navbar navbar-horizontal navbar-expand navbar-dark flex-row align-items-md-center ct-navbar bg-primary py-2 kpw-header"
-            >
+        .kpw-header-logo {
+            font-size: 20px;
+            color: white !important;
+        }
 
-                <a class="kpw-header-logo" href="/" class="">
-                    <?= $conf['title'] ?>
-                </a>
+        .kpw-header-logo:hover {
+            text-decoration: none !important;
+            color: #eee !important;
+        }
 
-                <div class="d-none d-sm-block ml-auto">
-                    <ul class="navbar-nav ct-navbar-nav flex-row align-items-center">
+        .kpw-footer {
+            background-color: #343a40 !important;
+            border-radius: 0 !important;
+            flex-shrink: 0 !important;
+        }
 
-<?php
-$menu_items = (new \dokuwiki\Menu\UserMenu())->getItems();
-foreach ($menu_items as $item) {
-    echo '<li>'
-        .'<a class="nav-link" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
-        .'<i class="argon-doku-navbar-icon">'.inlineSVG($item->getSvg()).'</i>'
-        . '<span class="a11y">'.$item->getLabel().'</span>'
-        . '</a></li>';
-}
-?>
+        .kpw-footer svg {
+            fill: white !important;
+        }
+
+        .kpw-footer svg:hover {
+            fill: #ccc !important;
+        }
+
+        .kpw-footer li:hover {
+            fill: #ccc !important;
+        }
+
+        .kpw-footer a:hover {
+            color: #ccc !important;
+            fill: #ccc !important;
+        }
+
+        .kpw-full-menu {
+            list-style-type: none;
+            display: inline !important;
+        }
+
+        .kpw-full-menu li {
+            display: inline !important;
+        }
+
+        .kpw-full-menu li a {
+            display: inline !important;
+        }
+
+        .kpw-content-wrapper {
+            flex: 1 0 auto !important;
+        }
+    </style>
+
+</head>
+
+<body class="docs ">
+    <div class="kpw-content-wrapper" id="dokuwiki__site">
 
 
-                        <li class="nav-item">
-                            <div class="search-form">
-                                <?php tpl_searchform()?>
-                            </div>
-                        </li>
+        <header class="navbar navbar-horizontal navbar-expand navbar-dark flex-row align-items-md-center ct-navbar bg-primary py-2 kpw-header">
+
+            <a class="kpw-header-logo" href="/" class="">
+                <?= $conf['title'] ?>
+            </a>
+
+            <div class="d-none d-sm-block ml-auto">
+                <ul class="navbar-nav ct-navbar-nav flex-row align-items-center">
+
+                    <?php
+                    $menu_items = (new \dokuwiki\Menu\UserMenu())->getItems();
+                    foreach ($menu_items as $item) {
+                        echo '<li>'
+                            . '<a class="nav-link" href="' . $item->getLink() . '" title="' . $item->getTitle() . '">'
+                            . '<i class="argon-doku-navbar-icon">' . inlineSVG($item->getSvg()) . '</i>'
+                            . '<span class="a11y">' . $item->getLabel() . '</span>'
+                            . '</a></li>';
+                    }
+                    ?>
 
 
-                    </ul>
-                </div>
-                <button class="navbar-toggler ct-search-docs-toggle d-block d-md-none ml-auto ml-sm-0" type="button"
-                    data-toggle="collapse" data-target="#kpw-table-of-contents" aria-controls="ct-docs-nav" aria-expanded="false"
-                    aria-label="Toggle docs navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-            </header>
+                    <li class="nav-item">
+                        <div class="search-form">
+                            <?php tpl_searchform() ?>
+                        </div>
+                    </li>
 
 
+                </ul>
+            </div>
+            <button class="navbar-toggler ct-search-docs-toggle d-block d-md-none ml-auto ml-sm-0" type="button" data-toggle="collapse" data-target="#kpw-table-of-contents" aria-controls="ct-docs-nav" aria-expanded="false" aria-label="Toggle docs navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            <div class="container-fluid">
-                <div class="row flex-xl-nowrap">
-
-
-<?php
-// Render the content initially
-ob_start();
-tpl_content(false);
-$buffer = ob_get_clean();
-?>
+        </header>
 
 
-                    <!-- center content -->
-                    <main class="col-12 col-md-12 offset-lg-1 col-lg-10 offset-xl-2 col-xl-8 py-md-3 pl-md-5 ct-content" role="main">
 
-                        <div id="dokuwiki__top" class="site
+        <div class="container-fluid">
+            <div class="row flex-xl-nowrap">
+
+
+                <?php
+                // Render the content initially
+                ob_start();
+                tpl_content(false);
+                $buffer = ob_get_clean();
+                ?>
+
+
+                <!-- center content -->
+                <main class="col-12 col-md-12 offset-lg-1 col-lg-10 offset-xl-2 col-xl-8 py-md-3 pl-md-5 ct-content" role="main">
+
+                    <div id="dokuwiki__top" class="site
                         <?php echo tpl_classes(); ?>
                         <?php echo ($showSidebar) ? 'hasSidebar' : ''; ?>">
-                        </div>
+                    </div>
 
-                        <?php html_msgarea()?>
-                        <?php tpl_includeFile('header.html')?>
+                    <?php html_msgarea() ?>
+                    <?php tpl_includeFile('header.html') ?>
 
 
-                        <!-- Trace/Navigation -->
-                        <nav aria-label="breadcrumb" role="navigation">
-                            <ol class="breadcrumb">
-                                <?php if ($conf['youarehere']) {?>
-                                <div class="breadcrumbs"><?php tpl_youarehere()?></div>
-                                <?php }?>
-                                <?php if ($conf['breadcrumbs']) {?>
-                                <div class="breadcrumbs"><?php tpl_breadcrumbs()?></div>
-                                <?php }?>
-                            </ol>
-                        </nav>
+                    <!-- Trace/Navigation -->
+                    <nav aria-label="breadcrumb" role="navigation">
+                        <ol class="breadcrumb">
+                            <?php if ($conf['youarehere']) { ?>
+                                <div class="breadcrumbs"><?php tpl_youarehere() ?></div>
+                            <?php } ?>
+                            <?php if ($conf['breadcrumbs']) { ?>
+                                <div class="breadcrumbs"><?php tpl_breadcrumbs() ?></div>
+                            <?php } ?>
+                        </ol>
+                    </nav>
 
-                        <div id="kpw-table-of-contents" class="collapse navbar-collapse kpw-table-of-contents">
-                            <?php tpl_toc()?>
-                        </div>
+                    <div id="kpw-table-of-contents" class="collapse navbar-collapse kpw-table-of-contents">
+                        <?php tpl_toc() ?>
+                    </div>
 
-                        <!-- Wiki Contents -->
-                        <div id="dokuwiki__content">
-                            <div class="pad">
+                    <!-- Wiki Contents -->
+                    <div id="dokuwiki__content">
+                        <div class="pad">
 
-                                <div class="page">
+                            <div class="page">
 
-                                    <?php echo $buffer ?>
-                                </div>
-                            </div>							
-                        </div>
-
-                        <hr />
-
-                        <?php tpl_indexerWebBug(); ?>
-                    </main>
-
-                </div>
-            </div>
-        </div>
-                        <!-- Footer -->
-                        <div class="card footer-card kpw-footer">
-                            <div class="card-body">
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col">
-                                            <div id="dokuwiki__footer">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-
-                                        <div class="footer-search">
-                                            <?php tpl_searchform()?>
-                                        </div>
-
-                                    </div>
-                                    <br/>
-                                    <div class="row" style="justify-content: center">
-                                    <ul class="kpw-full-menu aargon-doku-footer-fullmenu">
-                                        <?php
-                                        $menu_items = (new \dokuwiki\Menu\MobileMenu())->getItems();
-                                        foreach ($menu_items as $item) {
-                                            echo '<li>'
-                                                .'<a class="" href="'.$item->getLink().'" title="'.$item->getTitle().'">'
-                                                .'<i class="">'.inlineSVG($item->getSvg()).'</i>'
-                                                . '<span class="a11y">'.$item->getLabel().'</span>'
-                                                . '</a></li>';
-                                        }
-                                        ?>
-                                    </ul>
-                                    <?php tpl_includeFile('footer.html') ?>
-                                    </div>
-
-                                </div>
-
+                                <?php echo $buffer ?>
                             </div>
                         </div>
-    </body>
+                    </div>
+
+                    <hr />
+
+                    <?php tpl_indexerWebBug(); ?>
+                </main>
+
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+    <div class="card footer-card kpw-footer">
+        <div class="card-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div id="dokuwiki__footer">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="footer-search">
+                        <?php tpl_searchform() ?>
+                    </div>
+
+                </div>
+                <br />
+                <div class="row" style="justify-content: center">
+                    <ul class="kpw-full-menu aargon-doku-footer-fullmenu">
+                        <?php
+                        $menu_items = (new \dokuwiki\Menu\MobileMenu())->getItems();
+                        foreach ($menu_items as $item) {
+                            echo '<li>'
+                                . '<a class="" href="' . $item->getLink() . '" title="' . $item->getTitle() . '">'
+                                . '<i class="">' . inlineSVG($item->getSvg()) . '</i>'
+                                . '<span class="a11y">' . $item->getLabel() . '</span>'
+                                . '</a></li>';
+                        }
+                        ?>
+                    </ul>
+                    <?php tpl_includeFile('footer.html') ?>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</body>
 
 </html>
